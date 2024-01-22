1a7e56e8ab2562142e9504a57cc0f332bb08e39e
Dimitar Dzhambazov <129491664+dzambazov1986@users.noreply.github.com>
2024-01-22 11:09:44 +0200

    P2
    Four Operations

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿double USD  = double.Parse(Console.ReadLine());
-double EUR = USD * 0.88;
-Console.WriteLine($"{EUR:F2}");
+﻿using System;
+
+double firstNumber = double.Parse(Console.ReadLine());
+double secondNumber = double.Parse(Console.ReadLine());
+
+double adding = firstNumber + secondNumber;
+double subtraction = firstNumber - secondNumber;    
+double multiplication = firstNumber * secondNumber; 
+double division = firstNumber / secondNumber;
+
+Console.WriteLine($"{firstNumber:F2} + {secondNumber:F2} = {adding:F2}");
+Console.WriteLine($"{firstNumber:F2} - {secondNumber:F2} = {subtraction:F2}");
+Console.WriteLine($"{firstNumber:F2} * {secondNumber:F2} = {multiplication:F2}");
+Console.WriteLine($"{firstNumber:F2} / {secondNumber:F2} = {division:F2}");
+
